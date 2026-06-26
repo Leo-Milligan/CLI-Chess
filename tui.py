5bbcac3e31c04e23e53b2cff66dcc63976264c48
Leo Milligan <leomilligan20@gmail.com>
2026-06-26 22:44:09 +0100

    Integrated the json file for colour data.

--- tui.py
+++ tui.py
@@ -2,34 +2,25 @@
 
 from board import chess_board
 
+import json
+
 from textual.app import App
 from textual.containers import Grid
 from textual.widgets import Footer, Header, Static
 from textual.color import Color
 
-piece_symbols = {"white": {"king": "♔",
-                           "queen": "♕",
-                           "rook": "♖",
-                           "bishop": "♗",
-                           "knight": "♘",
-                           "pawn": "♙"
-                           },
-
-                 "black": {"king": "♚",
-                           "queen": "♛",
-                           "rook": "♜",
-                           "bishop": "♝",
-                           "knight": "♞",
-                           "pawn": "♟"}
-                 }
+with open("ui_style_sheet.json") as data:
+    ui_style_sheet = json.load(data)
 
 class ChessBoardGrid(Grid):
 
-    def __init__(self, board, num_rows, num_cols):
+    def __init__(self, board, num_rows, num_cols, piece_symbols, board_colour):
         super().__init__(id = "chess_board_grid")
+        self.board = board
         self.num_rows = num_rows
         self.num_cols = num_cols
-        self.board = board
+        self.piece_symbols = piece_symbols
+        self.board_colour = board_colour
 
     def compose(self):
 
@@ -43,17 +34,27 @@ class ChessBoardGrid(Grid):
 
                 if cell_contents == None:
                     piece_symbol = ""
+                    piece_colour = None
                 else:
                     piece_name = type(cell_contents).__name__
                     piece_colour = cell_contents.colour
-                    piece_symbol = piece_symbols[piece_colour][piece_name]
+                    piece_symbol = ui_style_sheet["piece_symbols"][self.piece_symbols][piece_name]
 
                 cell = Static(piece_symbol, classes="cell")
 
+                if piece_colour == "black":
+                    r,g,b = ui_style_sheet["board_colours"][self.board_colour]["dark_piece_colour"]
+                    cell.styles.color = Color(r, g, b)
+                elif piece_colour == "white":
+                    r,g,b = ui_style_sheet["board_colours"][self.board_colour]["light_piece_colour"]
+                    cell.styles.color = Color(r, g, b)
+
                 if (row + col) % 2:
-                    cell.styles.background = Color(150, 100, 60)
+                    r,g,b = ui_style_sheet["board_colours"][self.board_colour]["dark_square_colour"]
+                    cell.styles.background = Color(r, g, b)
                 else:
-                    cell.styles.background = Color(210, 195, 180)
+                    r,g,b = ui_style_sheet["board_colours"][self.board_colour]["light_square_colour"]
+                    cell.styles.background = Color(r, g, b)
 
                 yield cell
 
@@ -73,7 +74,10 @@ class ChessApp(App):
         num_cols = self.chess_board.num_cols
         board = self.chess_board.board
 
-        yield ChessBoardGrid(board, num_rows, num_cols)
+        piece_style = "small"
+        board_colour = "default"
+
+        yield ChessBoardGrid(board, num_rows, num_cols, piece_style, board_colour)
 
     def on_mount(self):
 
